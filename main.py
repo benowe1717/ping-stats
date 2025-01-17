7341382d053075674a04726b857689bec4d7b612
Benjamin Owen <benjamin@projecttiy.com>
2025-01-17 12:29:46 -0500

    created ParseArgs class to handle accepting arguments, added constants file to help supply all constants to all classes and scripts, updated main.py to use new parseargs class

--- main.py
+++ main.py
@@ -3,9 +3,19 @@
 Gather statistics about all IPs in your route to a destination
 """
 
+import sys
+
+from src.classes.parseargs import ParseArgs
+
 
 def main():
     """Main program loop"""
+    args = sys.argv
+    parseargs = ParseArgs(args)
+
+    if len(parseargs.ips) == 0:
+        print('ERROR: No IPs found!')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
