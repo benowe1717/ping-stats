ba18bb99744e1709b95b84fbe9d28c780744b553
Benjamin Owen <benjamin@projecttiy.com>
2025-01-17 11:52:37 -0500

    skeleton project created

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Gather statistics about all IPs in your route to a destination
+"""
+
+
+def main():
+    """Main program loop"""
+
+
+if __name__ == '__main__':
+    main()
