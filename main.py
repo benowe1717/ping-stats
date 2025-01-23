07081387c6f54bacbb883a8788404690be5c9538
Benjamin Owen <benjamin@projecttiy.com>
2025-01-23 16:58:23 -0500

    added new MTR class to be a wrapper around the mtr binary in report mode, added unit tests for MTR class, updated main script to use MTR class

--- main.py
+++ main.py
@@ -6,6 +6,48 @@ Gather statistics about all IPs in your route to a destination
 import sys
 
 from src.classes.parseargs import ParseArgs
+from src.classes.mtr import MTR
+
+
+def combine_traces(traces: list) -> dict:
+    """
+    Take all traces and combine them into a single dictionary.
+    """
+    combined_traces = {}
+    for trace in traces:
+        ip = trace['ip_addr']
+
+        if ip in combined_traces:
+            for key, value in trace.items():
+                if key != 'ip_addr':
+                    combined_traces[ip][key].append(value)
+
+        else:
+            combined_traces.update({ip: {}})
+            for key, value in trace.items():
+                if key != 'ip_addr':
+                    combined_traces[ip][key] = [value]
+
+    return combined_traces
+
+
+def calculate_averages(traces: dict) -> dict:
+    """
+    Loop through the dictionary and find all instances of IPs with multiple
+    values. Make the multiple values a single value through averages.
+    """
+    for ip, trace in traces.items():
+        for key, value in trace.items():
+            if len(value) > 1:
+                count = len(value)
+                total = sum(value)
+                average = round((total / count), 2)
+                trace[key] = average
+
+            elif len(value) == 1:
+                trace[key] = value[0]
+
+    return traces
 
 
 def main():
@@ -16,6 +58,20 @@ def main():
     if len(parseargs.ips) == 0:
         print('ERROR: No IPs found!')
         sys.exit(1)
+
+    traces = []
+    mtr = MTR(parseargs.ips)
+    for ip in mtr.ips:
+        mtr.run_mtr(ip)
+        results = mtr.parse_output()
+        if len(results) == 0:
+            continue
+
+        for result in results:
+            traces.append(result)
+
+    combined_traces = combine_traces(traces)
+    averaged_traces = calculate_averages(combined_traces)
 
 
 if __name__ == '__main__':
