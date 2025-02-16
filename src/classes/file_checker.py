8d9d766ef84a764eca44ace7b6ca02427ccb558c
Benjamin Owen <78978715+benowe1717@users.noreply.github.com>
2025-02-16 11:48:09 -0500

    reworked filechecker class with better commenting and consistency, added helpful error message when mtr is not installed (#13)

--- src/classes/file_checker.py
+++ src/classes/file_checker.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-Helper methods to determine if a file is preset, readable, writable, or any
-other properties associated with files and folder
+FileChecker() class file
 """
 import json
 import os
@@ -12,8 +11,8 @@ import yaml
 
 class FileChecker():
     """
-    Helper methods to determine if a file is preset, readable, writable, or any
-    other properties associated with files and folder
+    Determine and obtain several attributes associated with files and
+    directories.
     """
 
     def __init__(self, file: str) -> None:
@@ -30,7 +29,10 @@ class FileChecker():
 
     def is_file(self) -> bool:
         """
-        Determine if the given object is a file
+        Determine if self.file is a file
+
+        :return: True if is file, False if not file
+        :rtype: bool
         """
         if os.path.isfile(self.file):
             return True
@@ -38,7 +40,10 @@ class FileChecker():
 
     def is_dir(self) -> bool:
         """
-        Determine if the given object is a folder
+        Determine if self.file is a folder
+
+        :return: True if is directory, False if not directory
+        :rtype: bool
         """
         if os.path.isdir(self.file):
             return True
@@ -46,8 +51,10 @@ class FileChecker():
 
     def is_readable(self) -> bool:
         """
-        Determine if the given object is readable by the user executing the
-        script
+        Determine if self.file is readable by the user executing the program
+
+        :return: True if readable, False if not readable
+        :rtype: bool
         """
         if os.access(self.file, os.R_OK):
             return True
@@ -55,8 +62,10 @@ class FileChecker():
 
     def is_writable(self) -> bool:
         """
-        Determine if the given object is writable by the user executing the
-        script
+        Determine if self.file is writable by the user executing the program
+
+        :return: True if writable, False if not writable
+        :rtype: bool
         """
         if os.access(self.file, os.W_OK):
             return True
@@ -64,8 +73,11 @@ class FileChecker():
 
     def is_yaml(self) -> bool:
         """
-        Determine if the given object is a valid YAML object
-        Sets the self.yaml property if true
+        Determine if self.file contains valid YAML data
+        If True, read contents of self.file into self.yaml property
+
+        :return: True if valid YAML, False if not valid YAML
+        :rtype: bool
         """
         try:
             with open(self.file, 'r', encoding='utf-8') as f:
@@ -79,8 +91,11 @@ class FileChecker():
 
     def is_json(self) -> bool:
         """
-        Determine if the given object is a valid JSON object
-        Sets the self.json property if true
+        Determine if self.file contains valid JSON data
+        If True, read contents of self.file into self.json property
+
+        :return: True if valid JSON, False if not valid JSON
+        :rtype: bool
         """
         try:
             with open(self.file, 'r', encoding='utf-8') as f:
