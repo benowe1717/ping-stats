c41dfad1b6aad41d3b26967e51d0673d3a0815db
Benjamin Owen <benjamin@projecttiy.com>
2025-02-26 22:31:24 -0500

    version bump

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -5,7 +5,7 @@
 ARGPARSE_PROGRAM_NAME = 'main.py'
 ARGPARSE_PROGRAM_DESCRIPTION = 'A program to identify all IP Addresses and '
 ARGPARSE_PROGRAM_DESCRIPTION += 'their latency from you to your destination.'
-ARGPARSE_VERSION = '0.0.1'
+ARGPARSE_VERSION = '1.0.0'
 ARGPARSE_AUTHOR = 'Benjamin Owen'
 ARGPARSE_REPO = 'https://github.com/benowe1717/ping-stats'
 
